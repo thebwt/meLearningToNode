db5917630977dab38747585b027c5292e83271e2
Brandon Tomlinson <Brandon.Tomlinson@rackspace.com>
2016-06-28 18:17:22 -0500

    Breaking out the static file handler

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Post = require('./models/post')
 
 var app = express();
 app.use(bodyParser.json());
-app.use('/api/posts',require('./controllers/api/posts'))
 
-app.get('/', function (req, res) {
-	res.sendfile('layouts/posts.html')
-})
+app.use('/api/posts',require('./controllers/api/posts'))
+app.use('/', require('./controllers/static'))
+
 app.listen(8888,function() {
 	console.log('Server Listening on', 8888)
 })
