5d48b5b94f079bffdd3eb1825e425648372d4649
Brandon Tomlinson <brandon.tomlinson@rackspace.com>
2016-06-28 09:21:40 -0500

    Hellow world redone in express

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var http = require('http')
-http.createServer(function(request,response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
-}).listen(3000);
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res) {
+	res.status(200).send('Hello World');
+});
+
+app.listen(8888);
