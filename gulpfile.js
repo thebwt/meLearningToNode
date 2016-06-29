2150d7497866ec395c41330a462cd930473390c1
Brandon Tomlinson <Brandon.Tomlinson@rackspace.com>
2016-06-28 21:15:07 -0500

    Moving assets/app.js to be built by gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+
+gulp.task('js', function() {
+	gulp.src('ng/**/*.js')
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('assets'))
+		})
+
