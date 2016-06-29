b97843bb90bba6cc0186a6abbdffe3560e0f5ddc
Brandon Tomlinson <brandon.tomlinson@rackspace.com>
2016-06-29 14:29:52 -0500

    Angular router

--- controllers/static.js
+++ controllers/static.js
@@ -2,9 +2,10 @@ var express = require('express')
 var router = express.Router()
 
 router.use(express.static(__dirname + '/../assets'))
+router.use(express.static(__dirname + '/../templates'))
 
 router.get('/', function (req, res) {
-	res.sendFile('layouts/posts.html', { root: __dirname+'/..'})
+	res.sendFile('layouts/app.html', { root: __dirname+'/..'})
 })
 
 module.exports = router
